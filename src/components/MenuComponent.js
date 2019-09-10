63f695a54ea2c51dc01dd7c9bc307af216d966f6
Aditi Bhat <aditiabhat@gmail.com>
2019-09-10 16:28:21 +0530

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle  } from 'reactstrap';
+import DishDetail from './DishDetailComponent.js'
 
 class Menu extends Component {
 	constructor(props){
 		super(props);
 		this.state={
 			selectedDish: null
-		}
+      }
+      console.log('Menu Component constructor is invoked');
    }
    
+   componentDidMount() {
+      console.log('Menu Component componentDidMount is invoked');
+   }
    onDishSelect(dish) {
       this.setState({ selectedDish: dish});
    }
@@ -45,15 +50,13 @@ class Menu extends Component {
 				</div>
 			);
 		});
-
+      console.log('Menu Component render is invoked');
 		return ( 
 			<div className="container">
 				<div className="row">
 					{menu}
 				</div>
-            <div className="row">
-               {this.renderDish(this.state.selectedDish)}
-            </div>
+            <DishDetail selectedDish={this.state.selectedDish}/>
 			</div>
 		);
 	}

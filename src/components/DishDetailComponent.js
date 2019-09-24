2d092c1d1b1ecc167ac0ca8c1c659829e5581cb2
Aditi Bhat <aditiabhat@gmail.com>
2019-09-24 14:49:10 +0530

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import { Link } from 'react-router-dom';
 import { Button, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -88,7 +89,26 @@ const DishDetail = (props) => {
     console.log(props.dish)
 
 
-    if (props.dish != null) {
+
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
@@ -113,6 +133,7 @@ const DishDetail = (props) => {
             </div>
         )
     }
+
     else {
         return (
             <div></div>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, Breadcrumb,BreadcrumbItem,CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, Breadcrumb, BreadcrumbItem, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 function RenderMenuItem({ dish, onClick }) {
 	return (
 		<Card>
@@ -19,32 +19,52 @@ const Menu = (props) => {
 
 	console.log(props)
 
-	const menu = props.dishes.map((dish) => {
+	const menu = props.dishes.dishes.map((dish) => {
 		return (
 			<div key={dish.id} className="col-12 col-md-5 m-1">
-				<RenderMenuItem dish={dish}/>
+				<RenderMenuItem dish={dish} />
 			</div>
 		);
 	});
-	console.log('Menu Component render is invoked');
-	return (
-		<div className="container">
-			<div className="row">
-				<Breadcrumb>
-					<BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-					<BreadcrumbItem active>Menu</BreadcrumbItem>
-				</Breadcrumb>
-				<div className="col-12">
-					<h3>Menu</h3>
-					<hr/>
+	if (props.dishes.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
 				</div>
 			</div>
-			<div className="row">
-				{menu}
+		);
+	}
+	else if (props.dishes.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.dishes.errMess}</h4>
+				</div>
 			</div>
+		);
+	}
+	else {
 
-		</div>
-	);
+		return (
+			<div className="container">
+				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+						<BreadcrumbItem active>Menu</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>Menu</h3>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
+					{menu}
+				</div>
+
+			</div>
+		);
+	}
 }
 
 
